35cade5c042d1c405b2ffae7d1432baf9effcd3b
Uknown <abriansyahadam5@gmail.com>
2025-09-04 10:43:29 +0700

    Done with db connection

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
+with get_db_connection() as conn:
+    conn.execute('''CREATE TABLE IF NOT EXISTS Buku (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        judul TEXT NOT NULL,
+        penulis TEXT NOT NULL,
+        tahun_terbit INTEGER NOT NULL
+    )''')
+    conn.commit()
