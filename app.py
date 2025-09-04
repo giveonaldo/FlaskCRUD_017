34954fa392117adceb7c8ec92a37b38c1450d19a
Uknown <abriansyahadam5@gmail.com>
2025-09-04 10:41:41 +0700

    define connection database

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
+DATABASE = 'buku.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
